e9fbe323f9ed4fee3c0185944b9f3bb294e943b4
Jane Chang <jcl_chang@yahoo.com>
2018-09-26 18:21:26 -0700

    updated comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// from data.js
+// from data.js - give the dataset a more meaningful name
 var ufoData = data;
 
 // Select the "Filter Table" button
@@ -6,7 +6,6 @@ var filterBtn = d3.select("#filter-btn");
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
-
 
 // handle "Filter Table" button click 
 filterBtn.on("click", function() {
@@ -18,44 +17,34 @@ filterBtn.on("click", function() {
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input box "Enter Date" 
+  // need this input date to search and return sightings on this date
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
   console.log(ufoData);
 
+  // filter to include only sightings matching the input date
   var filteredData = ufoData.filter(ufoSighting => ufoSighting.datetime === inputValue);
 
   console.log(filteredData);
 
+  // now for each sighting, need to add the data  to the table on page
   filteredData.forEach(function(ufoSighting) {
 	console.log(ufoSighting);
+	
+	// add a row in the table for each object
 	var row = tbody.append("tr");
+	
+	// now loop through all key/value pairs in the object to add cells and values to cells
 	Object.entries(ufoSighting).forEach(function ([key, value]) {
 		console.log(key, value);
 		
+		// append a cell for each key/valu pair
+		// what's displayed on the page is in the same order as data
+		// so the correct value will fall under the right column heading
 		var cell = tbody.append("td");
 		cell.text(value);
 	});
   });
   
-  // data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = tbody.append("td");
-//     cell.text(value);
-//   });
-// });
-
-  
-  // Finally, add the summary stats to the `ul` tag
-  //d3.select(".summary")
-   // .append("li").text(`Mean: ${mean}`)
-   // .append("li").text(`Median: ${median}`)
-   // .append("li").text(`Mode: ${mode}`)
-   // .append("li").text(`Variance: ${variance}`)
-   // .append("li").text(`Standard Deviation: ${standardDeviation}`);
 });
